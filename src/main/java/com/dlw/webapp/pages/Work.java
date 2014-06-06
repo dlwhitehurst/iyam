752de0dcefd7dad1836d4f1a1240698cc681fd78
David L. Whitehurst <dlwhitehurst@me.com>
2014-06-06 00:59:22 -0400

    Fixed up all menus and menu base pages
    Created java and tml for menu base pages.  Just page links to home page
    but with outline labels

---
+++ src/main/java/com/dlw/webapp/pages/Work.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2014 David L. Whitehurst
+ * 
+ * Licensed under the Apache License, Version 2.0 
+ * (the "License"); You may not use this file except 
+ * in compliance with the License. You may obtain a 
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the 
+ * License.
+ * 
+ */
+
+package com.dlw.webapp.pages;
+
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.RequestGlobals;
+import org.apache.tapestry5.services.Response;
+import org.slf4j.Logger;
+
+/**
+ * @author <a href="mailto:dlwhitehurst@me.com">David L. Whitehurst</a>
+ *
+ */
+public class Work {
+
+	@Inject
+    private Logger logger;
+
+    @Inject
+    private RequestGlobals globals;
+
+    @Inject
+    private Request request;
+
+    @Inject
+    private Response response;
+
+}
